2dd67e5e28b249f2eef244022cb73d5687a20626
saadyah <saadyahgarber@gmail.com>
2023-02-05 16:33:33 +0200

    updated styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,9 @@
-<p><a href="/">Home</a> <a href="/about">About</a></p>
-
-<slot></slot>
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li> <a href="/about">About</a></li>
+    </ul>
+</nav>
+<div id="app">
+    <slot></slot>
+</div>
